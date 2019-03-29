21a6fb629e5b32e747b727a0f090b9664b8f5b16
Stephan Hradek <github.mails.skeeve@xoxy.net>
2019-03-29 16:41:16 +0100

    Also create SIWECOS.php

---
+++ build/siwecos-business-layer/de/SIWECOS.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Language Lines
+  |--------------------------------------------------------------------------
+  |
+  | This is a generated file.
+  |
+  | Any manual change will be lost.
+  |
+  */
+
+  'REPORT_FOR'      => "Report für :domain",
+  'SCANNER_TIMEOUT' => "Der Scanner \"%scanner%\" wurde nach %timeoutvalue% Sekunden beendet, weil er kein Ergebnis geliefert hat.",
+
+];
