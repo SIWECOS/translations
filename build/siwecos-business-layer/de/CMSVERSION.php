9846eb6a7081099d6072c7e398037fb1501a8e56
Stephan Hradek <github.mails.skeeve@xoxy.net>
2019-03-29 16:30:04 +0100

    First Test-output of dir2lang

---
+++ build/siwecos-business-layer/de/CMSVERSION.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Language Lines
+  |--------------------------------------------------------------------------
+  |
+  | This is a generated file.
+  |
+  | Any manual change will be lost.
+  |
+  */
+
+  // These are the de texts for
+  'SCANNER_NAME' => "CMS Versions Scanner",
+
+  // Test: CMSVERSION
+  'CMSVERSION_HEADLINE'      => "Überprüfung der CMS Version",
+  'CMSVERSION_POSITIVE'      => "Das verwendete CMS ist aktuell.",
+  'CMSVERSION_NEGATIVE'      => "CMS Version veraltet",
+  'CMSVERSION_DESCRIPTION'   => "<p>Regelmäßige und zeitnahe CMS-Updates sind eine der effektivsten Möglichkeiten, die eigene Website abzusichern. Kritische Lücken in den Systemen werden oft schon wenige Stunden nach dem Update massenhaft für Angriffe ausgenutzt, umso wichtiger ist hier die kontinuerliche Pflege des Systems. </p>",
+  'CMSVERSION_SOLUTION_TIPS' => "<p>Aktualisieren Sie Ihre CMS-Installation. In der Regel sind kleinere Updates innerhalb des selben Versionzweigs (z. B. von 1.5 auf 1.6) per Mausklick aus der Administration des CMS heraus möglich. Größere Versionssprünge (z. B. von 1.5 auf 2.0) erfordern oftmals Anpassungen an der Seite und müssen vom Website-Entwickler durchgeführt werden. Vor jedem Update ist ein Backup der Seite zu erstellen. </p><p>Prüfen Sie Ihr CMS im Optimalfall mindestens 1 mal pro Tag auf etwaige Updates. Alternativ empfiehlt sich die Beauftragung eines Dienstleisters im Rahmen einer Wartungsvereinbarung. </p>",
+  'CMSVERSION_LINK'          => "https://siwecos.de/wiki/CMS Versions Scanner/DE",
+
+  // Result texts follow
+  'CMS_CANT_DETECT_CMS'     => "Das von Ihnen verwendete Content Management System (%cms%) konnte nicht erkannt werden.",
+  'CMS_CANT_DETECT_VERSION' => "Die exakte Version des von Ihnen verwendete Content Management System (%cms%) konnte nicht erkannt werden. Bitte beachten Sie dass dies nicht bedeutet, dass eine aktuelle Version installiert sein muss. Bitte prüfen Sie die Version manuell.",
+  'CMS_OUTDATED'            => "Das von Ihnen verwendete Content Management System (%cms%) ist veraltet! Installiert ist (%version%), bitte aktualisieren Sie auf die aktuelle Vesion (%latest%).",
+  'CMS_OUT_OF_SUPPORT'      => "Das von Ihnen verwendete Content Management System (%cms%) ist massiv veraltet und erhält keine weiteren Updates vom Herstellter. Bitte wechseln Sie auf einen neueren Versionszweig!",
+  'CMS_UPTODATE'            => "Das von Ihnen verwendete Content Management System (%cms%) ist in der aktuellen Version (%version%) installiert.",
+
+];
